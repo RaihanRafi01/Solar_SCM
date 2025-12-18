27a7038ed17c43e52bbb9baf316e9e7cb1d899ed
Raihan <raihanrafi.992@gmail.com>
2025-12-18 11:08:19 +0600

    readme file added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:scube_task/app/core/bindings/initial_binding.dart';
 import 'package:scube_task/app/core/themes/app_theme.dart';
 import 'package:scube_task/app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  // 2. Ensure bindings are initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 3. Set preferred orientations
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
   runApp(const MainApp());
 }
 
